07ec81ed90bb0a41285dbcc32b6bb435935652d4
BauerBank <94458982+BauerBank@users.noreply.github.com>
2022-12-26 17:44:51 +0100

    commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+
+        let x = x * 2;
+        println!("The value of x ist {x}");
+    
+    }
+    println!("The value of x ist {x}");
+}
+
+//Data Types
